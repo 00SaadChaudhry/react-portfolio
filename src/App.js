a8956ddd8e7e863ad37a0abe4e0c1c1dcf766689
00Saad <00saadchaudhry@gmail.com>
2018-11-11 18:20:22 -0500

    Router-dom test working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
 import './scss/App.scss';
 
 import Head from './components/Head';
 import Foot from './components/Foot';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Head />
-        <Foot />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Head />
+          <Foot />
+          <Route exact path='/home' component={Home}/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
