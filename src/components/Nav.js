f0ad59ac1cf0d646de88a1ef72725cb4e0c311cb
00Saad <00saadchaudhry@gmail.com>
2018-11-13 15:11:30 -0500

    state nonsense

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
 //Array containing react-router-dom data for Link and Route
@@ -10,21 +10,38 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faBars} from '@fortawesome/free-solid-svg-icons';
 library.add(faBars);
 
-function Nav() {
-  const navLinks = links.map(item => {
-    return <li key={item.name}><Link to={item.path}>{item.name}</Link></li>
-  });
+/*function handleClick() {
+  console.log('clicked');
+}*/
 
-  return (
-    <nav>
-      <button>
-        <FontAwesomeIcon icon={faBars} className="menu" />
-      </button>
-      <ul>
-        {navLinks}
-      </ul>
-    </nav>
-  )
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked:'not clicked'
+    };
+  }
+  handleClick() {
+    this.setState.clicked = 'clicked';
+    console.log(this.state.clicked);
+  }
+  render() {
+
+    const navLinks = links.map(item => {
+        return <li key={item.name}><Link to={item.path}>{item.name}</Link></li>
+      });
+  
+    return (
+      <nav className="Nav">
+        <button onClick={this.handleClick.bind(this)}>
+          <FontAwesomeIcon icon={faBars} className="menu" />
+        </button>
+        <ul>
+          {navLinks}
+        </ul>
+      </nav>
+    )
+  }
 }
 
 export default Nav;
